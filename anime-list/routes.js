ef3fed6cb0db8799558fffbe001b24185de1ec08
feyreyrey <98060466+feyreyrey@users.noreply.github.com>
2023-12-10 19:47:01 -0500

    com

--- anime-list/routes.js
+++ anime-list/routes.js
@@ -8,35 +8,25 @@ function animeRoutes(app) {
     const anime = await dao.findAllAnime();
     res.json(anime);
   };
-  const findUserById = async (req, res) => {
-    const id = req.params.userId;
-    const user = await dao.findUserById(id);
-    res.json(user);
+  const findAnimeById = async (req, res) => {
+    const id = req.params.aniId;
+    const anime = await dao.findAnimeById(id);
+    res.json(anime);
   };
-  const findUserByUsername = async (req, res) => {
-    const username = req.params.username;
-    const user = await dao.findUserByUsername(username);
-    res.json(user);
+  const findAnimeByTitle = async (req, res) => {
+    const title = req.params.title;
+    const anime = await dao.findAnimeByTitle(title);
+    res.json(anime);
   }
-  const updateUser = async (req, res) => { };
-  const signup = async (req, res) => { };
-  const signin = async (req, res) => {
-    const { username, password } = req.body;
-    currentUser = await dao.findUserByCredentials(username, password);
-    res.json(currentUser);
+  const findAnimeByRank = async (req, res) => {
+    const rank = req.params.raked;
+    const anime = await dao.findAnimeById(rank);
+    res.json(anime);
   };
-
-  const signout = (req, res) => { };
-  const account = async (req, res) => { };
   app.post("/api/anime", createAnime);
   app.get("/api/anime", findAllAnime);
-  app.get("/api/users/:userId", findUserById);
-  app.get("/api/users/username/:username", findUserByUsername);
-  app.put("/api/users/:userId", updateUser);
-  app.delete("/api/anime/:animeId", deleteAnime);
-  app.post("/api/users/signup", signup);
-  app.post("/api/users/signin", signin);
-  app.post("/api/users/signout", signout);
-  app.post("/api/users/account", account);
+  app.get("/api/anime/:aniId", findAnimeById);
+  app.get("/api/anime/:title", findAnimeByTitle);
+  app.get("/api/anime/:ranked", findAnimeByRank);
 }
-export default UserRoutes;
+export default animeRoutes;
--- app.js
+++ app.js
@@ -2,6 +2,7 @@ import express from 'express';
 import cors from 'cors';
 import mongoose from "mongoose";
 import UserRoutes from "./users/routes.js";
+import animeRoutes from './anime-list/routes.js';
 
 mongoose.connect("mongodb://127.0.0.1:27017/animelist");
 import Hello from "./hello.js"
@@ -12,6 +13,7 @@ const app = express()
 app.use(cors());
 app.use(express.json());
 UserRoutes(app);
+animeRoutes(app);
 Hello(app)
 
 app.listen(4000)
