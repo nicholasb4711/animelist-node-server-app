22d5029b1405f312e6fa37a53032e1c800d6701c
nicholasb4711 <nicholasb4711@gmail.com>
2023-12-11 01:09:29 -0500

    Update routes.js

--- users/routes.js
+++ users/routes.js
@@ -2,13 +2,17 @@ import * as dao from "./dao.js";
 let currentUser = null;
 function UserRoutes(app) {
   const createUser = async (req, res) => { };
-  const deleteUser = async (req, res) => { };
+  const deleteUser = async (req, res) => { 
+    const id = req.params.id;
+    const status = await dao.deleteUser(id);
+    res.json(status);
+  };
   const findAllUsers = async (req, res) => {
     const users = await dao.findAllUsers();
     res.json(users);
   };
   const findUserById = async (req, res) => {
-    const id = req.params.userId;
+    const id = req.params.id;
     const user = await dao.findUserById(id);
     res.json(user);
   };
@@ -23,7 +27,13 @@ function UserRoutes(app) {
     const user = await dao.findUserByPassword(username);
     res.json(user);
   };
-  const updateUser = async (req, res) => { };
+  
+  const updateUser = async (req, res) => {
+    const id = req.params.id;
+    const newUser = req.body;
+    const status = await dao.updateUser(id, newUser);
+    res.json(status);
+   };
   const signup = async (req, res) => { };
   const signin = async (req, res) => {
     const { username, password } = req.body;
@@ -39,11 +49,10 @@ function UserRoutes(app) {
   app.post("/api/users/account", account);
   app.post("/api/users", createUser);
   app.get("/api/users", findAllUsers);
-  app.get("/api/users/:userId", findUserById);
+  app.get("/api/users/:id", findUserById);
   app.get("/api/users/username/:username", findByUsername);
-  app.get("/api/users/username/:username", findByPassword);
-  app.put("/api/users/:userId", updateUser);
-  app.delete("/api/users/:userId", deleteUser);
+  app.put("/api/users/:id", updateUser);
+  app.delete("/api/users/:id", deleteUser);
   app.post("/api/users/signup", signup);
   app.post("/api/users/signin", signin);
   app.post("/api/users/signout", signout);
